85fcb26480f9cfee0cc347deb95cde6bad276c99
David Kim <dhkim1211@gmail.com>
2016-05-14 17:44:12 +0200

    created server.js file, index.jade, messages.jade, css file. Users can post a new message and view all posted messages

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var app = express();
+var db;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+
+
+MongoClient.connect('mongodb://dhkim1211:nycda@ds011369.mlab.com:11369/messageboard', function(err, database) {
+	//..start the server
+	if (err) {
+		return console.log(err);
+	}
+	db = database;
+	app.listen(3000, function() {
+		console.log('listening on 3000');
+	})
+})
+
+
+
+
+app.get('/', function(req, res) {
+	res.render('index.jade');
+})
+
+app.post('/messages', function(req, res) {
+	db.collection('message').save(req.body, function(err, results) {
+		if (err) { return console.log(err);}
+		console.log('saved to database');
+
+		res.redirect('messages');
+	})
+})
+
+app.get('/messages', function(req, res) {
+	//cursor = mongo object
+	var cursor = db.collection('message').find().toArray(function(err, results) {
+		if (err) { return console.log(err);}
+
+		res.render('messages', {posts: results});
+	})
+})
+
